67f5360def79ed57d604f9b510df3210c72e22f1
임지훈 <lonelynight1026@gmail.com>
2024-05-24 14:48:53 +0900

    feat : Cinema 엔티티 구현

---
+++ src/main/java/com/example/cinemareservation/domain/cinema/Cinema.java
@@ -0,0 +1,27 @@
+package com.example.cinemareservation.domain.cinema;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "cinema")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor
+public class Cinema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cinema", targetEntity = CinemaMovie.class, cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<CinemaMovie> movies = new ArrayList<>();
+}
