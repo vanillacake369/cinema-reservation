e29c018bce84d4711e1ea13ac285c3fddaf11bde
임지훈 <lonelynight1026@gmail.com>
2024-05-24 11:44:40 +0900

    Initial commit

---
+++ src/main/java/com/example/cinemareservation/CinemaReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemareservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaReservationApplication.class, args);
+    }
+
+}
