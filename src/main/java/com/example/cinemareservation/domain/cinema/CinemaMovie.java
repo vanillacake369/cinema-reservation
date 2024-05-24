f047667c615ab7ec194a89903b400221d2c4a1d1
임지훈 <lonelynight1026@gmail.com>
2024-05-24 14:49:34 +0900

    feat : CinemaMovie 엔티티 구현

---
+++ src/main/java/com/example/cinemareservation/domain/cinema/CinemaMovie.java
@@ -0,0 +1,25 @@
+package com.example.cinemareservation.domain.cinema;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "cinema_movie")
+@Entity
+@Getter
+public class CinemaMovie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cinema_id")
+    private Cinema cinema;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+}
