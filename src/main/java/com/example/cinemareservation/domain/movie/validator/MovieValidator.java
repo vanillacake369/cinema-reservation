1261f70ef193a877a46dda6b6b478e47b8c619f9
임지훈 <lonelynight1026@gmail.com>
2024-05-25 12:18:08 +0900

    feat : MovieValidator > 초기화

--- src/main/java/com/example/cinemareservation/domain/movie/validator/MovieValidator.java
+++ src/main/java/com/example/cinemareservation/domain/movie/validator/MovieValidator.java
@@ -1,4 +1,17 @@
 package com.example.cinemareservation.domain.movie.validator;
 
+import com.example.cinemareservation.domain.cinema.dto.request.ReservationRequestDto;
+import com.example.cinemareservation.domain.movie.infra.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class MovieValidator {
+
+	private final MovieRepository movieRepository;
+
+	public void validate(ReservationRequestDto requestDto) {
+
+	}
 }
