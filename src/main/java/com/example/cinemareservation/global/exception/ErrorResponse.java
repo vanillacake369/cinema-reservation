ea34f727df98da43fcbabf41ca7c22cdaf729ee2
임지훈 <lonelynight1026@gmail.com>
2024-05-25 13:18:05 +0900

    fix : ErrorResponse > google java style 에 맞춰 format

--- src/main/java/com/example/cinemareservation/global/exception/ErrorResponse.java
+++ src/main/java/com/example/cinemareservation/global/exception/ErrorResponse.java
@@ -1,27 +1,25 @@
 package com.example.cinemareservation.global.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
 import lombok.Builder;
-import lombok.Getter;
 import org.springframework.validation.FieldError;
 
-import java.util.List;
-
-@Getter
 @Builder
 public record ErrorResponse(
-        Integer code,
-        String message,
-        @JsonInclude(JsonInclude.Include.NON_EMPTY) List<ValidationError> errors
+	Integer code,
+	String message,
+	@JsonInclude(JsonInclude.Include.NON_EMPTY) List<ValidationError> errors
 ) {
-    @Getter
-    @Builder
-    public record ValidationError(String field, String message) {
-        public static ValidationError of(final FieldError fieldError) {
-            return ValidationError.builder()
-                    .field(fieldError.getField())
-                    .message(fieldError.getDefaultMessage())
-                    .build();
-        }
-    }
+
+	@Builder
+	public record ValidationError(String field, String message) {
+
+		public static ValidationError of(final FieldError fieldError) {
+			return ValidationError.builder()
+				.field(fieldError.getField())
+				.message(fieldError.getDefaultMessage())
+				.build();
+		}
+	}
 }
