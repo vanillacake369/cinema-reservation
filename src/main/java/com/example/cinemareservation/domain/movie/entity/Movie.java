51e1b9ac83a548304f1cbe6f20caf4cb6d758f37
임지훈 <lonelynight1026@gmail.com>
2024-05-25 13:18:30 +0900

    feat : Movie 엔티티 구현

--- src/main/java/com/example/cinemareservation/domain/movie/entity/Movie.java
+++ src/main/java/com/example/cinemareservation/domain/movie/entity/Movie.java
@@ -2,26 +2,30 @@ package com.example.cinemareservation.domain.movie.entity;
 
 import com.example.cinemareservation.domain.cinema.entity.CinemaMovie;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Entity
 @Table(name = "movie")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@RequiredArgsConstructor
 public class Movie {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "movie", targetEntity = CinemaMovie.class, cascade = CascadeType.ALL, orphanRemoval = true)
-    private final List<CinemaMovie> movies = new ArrayList<>();
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "movie", targetEntity = CinemaMovie.class, cascade = CascadeType.ALL, orphanRemoval = true)
+	private final List<CinemaMovie> movies = new ArrayList<>();
 }
