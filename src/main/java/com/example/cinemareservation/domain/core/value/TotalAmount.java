0eaaa3e644f8a0fe843851fd5aa8c4b609815e32
임지훈 <lonelynight1026@gmail.com>
2024-05-25 13:19:16 +0900

    feat : TotalAmount > 초기화

---
+++ src/main/java/com/example/cinemareservation/domain/core/value/TotalAmount.java
@@ -0,0 +1,8 @@
+package com.example.cinemareservation.domain.core.value;
+
+/**
+ * Value Object of
+ * "주문총액 {@link PaymentAmount} - 할인금액 {@link DiscountAmount}"
+ */
+public class TotalAmount {
+}
