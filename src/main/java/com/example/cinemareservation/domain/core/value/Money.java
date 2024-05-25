3ff232d379b6e719972b64e2a957bff9fcfec44e
임지훈 <lonelynight1026@gmail.com>
2024-05-25 18:33:10 +0900

    feat : Money 구현
    - validateAmount()
    - addAmount()
    - subAmount()
    - multiplyAmount()
    - divideAmount()

--- src/main/java/com/example/cinemareservation/domain/core/value/Money.java
+++ src/main/java/com/example/cinemareservation/domain/core/value/Money.java
@@ -1,27 +1,59 @@
 package com.example.cinemareservation.domain.core.value;
 
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public record Money(
-	@ValidMoneyArg BigDecimal amount
+	BigDecimal amount
 ) {
 
-	public Money addAmount(BigDecimal amountToAdd) {
-		if (amountToAdd == null || amountToAdd.signum() <= 0) {
-			throw new IllegalArgumentException("Amount to add must be positive and non-null");
-		}
+	/**
+	 * custom constructor with validation
+	 *
+	 * @throws IllegalArgumentException when input amount is null || zero || negative
+	 */
+	public Money {
+		validateAmount(amount);
+	}
 
-		BigDecimal newAmount = this.amount.add(amountToAdd);
+	private void validateAmount(BigDecimal amount) {
+		if (amount == null || amount.signum() <= 0) {
+			throw new IllegalArgumentException("Amount must be positive and non-null");
+		}
+	}
+
+	public Money addAmount(BigDecimal amount) {
+		validateAmount(amount);
+
+		BigDecimal newAmount = this.amount.add(amount);
 
 		return new Money(newAmount);
 	}
 
-	public Money subAmount(BigDecimal amountToAdd) {
-		if (amountToAdd == null || amountToAdd.signum() <= 0) {
-			throw new IllegalArgumentException("Amount to add must be positive and non-null");
-		}
+	public Money subAmount(@PositiveOrZero BigDecimal amount) {
+		validateAmount(amount);
 
-		BigDecimal newAmount = this.amount.subtract(amountToAdd);
+		BigDecimal newAmount = this.amount.subtract(amount);
+
+		return new Money(newAmount);
+	}
+
+	public Money multiplyAmount(@PositiveOrZero BigDecimal amount) {
+		validateAmount(amount);
+
+		BigDecimal newAmount = this.amount.multiply(amount);
+
+		return new Money(newAmount);
+	}
+
+	public Money divideAmount(@PositiveOrZero BigDecimal amount) {
+		validateAmount(amount);
+
+		// Divide precision of the IEEE 754-2019 decimal32 format
+		BigDecimal newAmount = this.amount.divide(amount, MathContext.DECIMAL32);
 
 		return new Money(newAmount);
 	}
--- src/test/java/com/example/cinemareservation/domain/core/value/MoneyTest.java
+++ src/test/java/com/example/cinemareservation/domain/core/value/MoneyTest.java
@@ -1,8 +1,8 @@
 package com.example.cinemareservation.domain.core.value;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import jakarta.validation.ConstraintViolationException;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,13 +13,13 @@ class MoneyTest {
 	@DisplayName("Money 의 생성자값으로 Negative 값이 들어온 경우 예외처리를 합니다.")
 	void 예외_Money_생성자초기화값_음수() {
 		// WHEN
-		ConstraintViolationException exception = assertThrows(ConstraintViolationException.class,
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
 			() -> {
 				Money money = new Money(BigDecimal.valueOf(-5000));
 			});
 
 		// THEN
-		System.out.println("exception.getMessage() = " + exception.getMessage());
+		assertEquals("Amount must be positive and non-null", exception.getMessage());
 	}
 
 	@Test
@@ -35,6 +35,54 @@ class MoneyTest {
 			});
 
 		// THEN
-		System.out.println("exception.getMessage() = " + exception.getMessage());
+		assertEquals("Amount must be positive and non-null", exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("Money.subAmount() 인자값으로 Negative 값이 들어온 경우 예외처리를 합니다.")
+	void 예외_Money_subAmount_인자값_음수() {
+		// GIVEN
+		Money money = new Money(BigDecimal.valueOf(5000));
+
+		// WHEN
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+			() -> {
+				money.subAmount(BigDecimal.valueOf(-5000));
+			});
+
+		// THEN
+		assertEquals("Amount must be positive and non-null", exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("Money.multiplyAmount() 인자값으로 Negative 값이 들어온 경우 예외처리를 합니다.")
+	void 예외_Money_multiplyAmount_인자값_음수() {
+		// GIVEN
+		Money money = new Money(BigDecimal.valueOf(5000));
+
+		// WHEN
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+			() -> {
+				money.multiplyAmount(BigDecimal.valueOf(-5000));
+			});
+
+		// THEN
+		assertEquals("Amount must be positive and non-null", exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("Money.divideAmount() 인자값으로 Negative 값이 들어온 경우 예외처리를 합니다.")
+	void 예외_Money_divideAmount_인자값_음수() {
+		// GIVEN
+		Money money = new Money(BigDecimal.valueOf(5000));
+
+		// WHEN
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+			() -> {
+				money.divideAmount(BigDecimal.valueOf(-5000));
+			});
+
+		// THEN
+		assertEquals("Amount must be positive and non-null", exception.getMessage());
 	}
 }
