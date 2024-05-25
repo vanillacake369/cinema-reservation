57390ce6bb12eb3deb87c0685261681955b85dfb
임지훈 <lonelynight1026@gmail.com>
2024-05-25 12:17:39 +0900

    fix : ReservationResponseDto > amount 에 대해 long 에서 bigdecimal 로 변경

--- src/main/java/com/example/cinemareservation/domain/cinema/dto/response/ReservationResponseDto.java
+++ src/main/java/com/example/cinemareservation/domain/cinema/dto/response/ReservationResponseDto.java
@@ -1,12 +1,14 @@
 package com.example.cinemareservation.domain.cinema.dto.response;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 public record ReservationResponseDto(
-        String movieName,
-        Timestamp timestamp,
-        Integer personCount,
-        Long originalPaymentAmount,
-        Long totalPaymentAmount
+	String movieName,
+	Timestamp timestamp,
+	Integer personCount,
+	BigDecimal originalPaymentAmount,
+	BigDecimal totalPaymentAmount
 ) {
+
 }
