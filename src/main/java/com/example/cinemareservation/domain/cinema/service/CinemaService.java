1c57431969e7ca616cb3498a3abaab22f686430b
임지훈 <lonelynight1026@gmail.com>
2024-05-25 12:09:00 +0900

    feat : CinemaService > 초기화

---
+++ src/main/java/com/example/cinemareservation/domain/cinema/service/CinemaService.java
@@ -0,0 +1,9 @@
+package com.example.cinemareservation.domain.cinema.service;
+
+import com.example.cinemareservation.domain.cinema.dto.response.ReservationResponseDto;
+
+public interface CinemaService {
+
+	ReservationResponseDto handleReservation();
+
+}
---
+++ src/main/java/com/example/cinemareservation/domain/cinema/service/CinemaServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.cinemareservation.domain.cinema.service;
+
+import com.example.cinemareservation.domain.cinema.dto.response.ReservationResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CinemaServiceImpl implements CinemaService{
+
+	@Override
+	public ReservationResponseDto handleReservation() {
+		return null;
+	}
+}
